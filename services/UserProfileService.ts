010e8567b2b18ed4ece80b218333b42d6dd5686c
Precious Ajayi <aspark400@gmail.com>
2025-05-23 23:22:00 +0100

    refacted user profile service and added profile service method for automatic match

---
+++ services/UserProfileService.ts
@@ -0,0 +1,111 @@
+import { Profile } from "@/types/profile";
+import axios from "axios";
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+if (!baseUrl)
+  throw new Error("NEXT_PUBLIC_BASE_URL environment variable is required");
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const handleError = (error: any) => {
+  if (axios.isAxiosError(error)) {
+    console.error("Axios error:", error.response?.data || error.message);
+    throw new Error(
+      error.response?.data?.message || "An unexpected error occurred."
+    );
+  } else {
+    console.error("Unexpected error:", error);
+    throw new Error("Something went wrong.");
+  }
+};
+
+export const patchWithParams = async <T>(
+  url: string,
+  id: string,
+  data: Record<string, string | number | boolean>
+): Promise<T> => {
+  try {
+    const res = await axios.patch(`${baseUrl}${url}`, data, {
+      params: { clerkId: id },
+    });
+    return res.data;
+  } catch (error) {
+    handleError(error);
+    return undefined as unknown as T;
+  }
+};
+
+export const userProfileService = {
+  async create(data: Profile, clerkId: string): Promise<Profile> {
+    try {
+      const res = await axios.post(`${baseUrl}/api/profiles/`, data, {
+        params: {
+          clerkId,
+        },
+      });
+      return res.data;
+    } catch (error) {
+      handleError(error);
+      return undefined as unknown as Profile;
+    }
+  },
+  async getAll(): Promise<Profile[]> {
+    try {
+      const res = await axios.get(`${baseUrl}/api/profiles/`);
+      return res.data;
+    } catch (error) {
+      handleError(error);
+      return undefined as unknown as Profile[];
+    }
+  },
+  async getById(id: string): Promise<Profile> {
+    try {
+      const res = await axios.get(`${baseUrl}/api/profiles/`, {
+        params: {
+          clerkId: id,
+        },
+      });
+      return res.data;
+    } catch (error) {
+      handleError(error);
+      return undefined as unknown as Profile;
+    }
+  },
+  async update(id: string, data: Partial<Profile>): Promise<Profile> {
+    try {
+      const res = await axios.patch(`${baseUrl}/api/profiles/modify`, data, {
+        params: {
+          clerkId: id,
+        },
+      });
+      return res.data;
+    } catch (error) {
+      handleError(error);
+      return undefined as unknown as Profile;
+    }
+  },
+  async updateUsername(
+    id: string,
+    data: Record<string, string | number | boolean>
+  ) {
+    return patchWithParams("/api/profiles/change-username", id, data);
+  },
+  async updateVisibility(
+    id: string,
+    data: Record<string, string | number | boolean>
+  ): Promise<string[]> {
+    return patchWithParams("/api/profiles/set-visibility", id, data);
+  },
+  async blockUser(
+    id: string,
+    data: Record<string, string | number | boolean>
+  ): Promise<string[]> {
+    return patchWithParams("api/profiles/block-contact", id, data);
+  },
+  async unblockUser(
+    id: string,
+    data: Record<string, string | number | boolean>
+  ): Promise<string[]> {
+    return patchWithParams("/api/profiles/unblock-contact", id, data);
+  },
+};
--- services/profileService.ts
+++ services/profileService.ts
@@ -1,67 +1,24 @@
-import { Profile } from "@/types/profile";
-import axios from "axios";
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+import axios from "axios";
+import { handleError } from "./UserProfileService";
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
 
 export const profileService = {
-  async create(data: Profile, clerkId: string): Promise<Profile> {
-    const res = await axios.post(`${baseUrl}/api/profiles/`, data, {
-      params: {
-        clerkId,
-      },
-    });
-    return res.data;
-  },
-  async getAll(): Promise<Profile[]> {
-    const res = await axios.get(`${baseUrl}/api/profiles/`);
-    return res.data;
-  },
-  async getById(id: string): Promise<Profile> {
-    const res = await axios.get(`${baseUrl}/api/profiles/`, {
-      params: {
-        clerkId: id,
-      },
-    });
-    return res.data;
-  },
-  async update(id: string, data: Partial<Profile>): Promise<Profile> {
-    const res = await axios.patch(`${baseUrl}/api/profiles/modify`, data,  {
-      params: {
-        clerkId: id,
-      },
-    });
-    return res.data;
-  },
-  async updateUsername(id: string, data: string[]) : Promise<string[]> {
-    const res = await axios.patch(`${baseUrl}/api/profiles/change-username`, data, {
-        params: {
-            clerkId: id
+    async getProfiles(id: string, distance: string | null, unit: string | null) {
+        try {
+            const res = await axios.get(`${baseUrl}/api/matching/auto_match`, {
+                params: {
+                    userId: id,
+                    maxDistance: distance,
+                    unit
+                }
+            })
+            return res.data
+        } catch(error) {
+            handleError(error)
         }
-    })
-    return res.data
-  },
-  async updateVisibility(id: string, data: string[]) : Promise<string[]> {
-    const res = await axios.patch(`${baseUrl}/api/profiles/set-visibility`, data, {
-        params: {
-            clerkId: id
-        }
-    })
-    return res.data
-  },
-  async blockUser(id: string, data: string[]) : Promise<string[]> {
-    const res = await axios.patch(`${baseUrl}/api/profiles/block-contact`, data, {
-        params: {
-            clerkId: id
-        }
-    })
-    return res.data
-  },
-  async unblockUser(id: string, data: string[]) : Promise<string[]> {
-    const res = await axios.patch(`${baseUrl}/api/profiles/unblock-contact`, data, {
-        params: {
-            clerkId: id
-        }
-    })
-    return res.data
-  }
-};
+    },
+    // There should be a service method for manual filtering but endpoint is structured unusual
+    
+}
